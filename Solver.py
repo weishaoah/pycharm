7d25d86954898e5e8cd6e9a9a9a01ed15343cbc0
weishaoah <shaow18@lzu.edu.cn>
2018-10-11 16:13:14 +0800

    a test file

---
+++ Solver.py
@@ -0,0 +1,11 @@
+import math
+
+
+class Solver(object):
+    def demo(self, a, b, c):
+        """
+
+        :rtype: object
+        """
+        d = b ** 2 - 4 * a * c
+        disc = math.sqrt(d)
